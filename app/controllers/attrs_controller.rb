294a10dc85bad9e1f536f7d1ee1fe096a0e8787d
iwtn <ganta.viii@gmail.com>
2018-11-18 22:01:29 +0900

    fix an updated path

--- app/controllers/attrs_controller.rb
+++ app/controllers/attrs_controller.rb
@@ -46,7 +46,7 @@ class AttrsController < ApplicationController
   def update
     respond_to do |format|
       if @attr.update(attr_params)
-        format.html { redirect_to @attr, notice: 'Attr was successfully updated.' }
+        format.html { redirect_to relation_attr_path, notice: 'Attr was successfully updated.' }
         format.json { render :show, status: :ok, location: @attr }
       else
         format.html { render :edit }
